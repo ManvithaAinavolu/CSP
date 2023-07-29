7741256694f2a992ff7aceae9f715b4e6c30ba0c
ManvithaAinavolu <manvithaainavolu@gmail.com>
2023-07-28 23:31:28 -0700

    commit

---
+++ Requires.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+const Feedback = require('./Routes/Feedback');
+const signup = require('./Routes/signup');
+const login=require('./Routes/login');
+const AR=require('./Routes/AR');
+const share1=require('./Routes/share1');
+const contact=require('./Routes/contact');
+const inspiration=require('./Routes/inspiration');
+const app = express();
+app.use(cors());
+app.use(bodyparser.urlencoded({ extended :  true}));
+app.use(bodyparser.json());
+app.use('/Feedback', Feedback);
+app.use('/signup', signup);
+app.use('/AR',AR);
+app.use('/inspiration', inspiration);
+app.use('/login', login);
+app.use('/share1', share1);
+app.use('/contact', contact);
+
+
+
+
+
+module.exports = app;
